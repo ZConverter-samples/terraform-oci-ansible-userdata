6e2bfb9e6d1f6ac0cb640cccf4af8343c27ac60f
ZConverter-samples <tykim@zconverter.com>
2022-11-09 13:30:07 +0900

    Add initial code

--- install.ps1
+++ install.ps1
@@ -31,6 +31,7 @@ elseif ($args[0] -like "apply")
     1. Server
     2. Linux client
     3. Windows client
+    4. Initial setup
     ---------------------------------------"
     $num = Read-Host "Select number: "
 
@@ -56,6 +57,33 @@ elseif ($args[0] -like "apply")
         # Excute
         .\terraform.exe -chdir=oci-ansible-client-windows/ init
         .\terraform.exe -chdir=oci-ansible-client-windows/ apply --var-file=oci_terraform_ansible_client_Windows.json -auto-approve
+    }
+    elseif ( $num -eq 4 )
+    {
+        $tenancy_ocid = Read-Host "Enter the tenancy_ocid "
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"', (Write-Output '"tenancy_ocid" : '$tenancy_ocid))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"', (Write-Output '"tenancy_ocid" : '$tenancy_ocid))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"', (Write-Output '"tenancy_ocid" : '$tenancy_ocid))} | Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
+        $user_ocid = Read-Host "Enter the user_ocid "
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"user_ocid" : "ocid1.user.oc1..~~~~"', (Write-Output '"user_ocid" : '$user_ocid))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"user_ocid" : "ocid1.user.oc1..~~~~"', (Write-Output '"user_ocid" : '$user_ocid))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"user_ocid" : "ocid1.user.oc1..~~~~"', (Write-Output '"user_ocid" : '$user_ocid))} | Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
+        $fingerprint = Read-Host "Enter the fingerprint "
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"fingerprint" : "~~~~"', (Write-Output '"fingerprint" : '$fingerprint))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"fingerprint" : "~~~~"', (Write-Output '"fingerprint" : '$fingerprint))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"fingerprint" : "~~~~"', (Write-Output 'fingerprint" : '$fingerprint))}| Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
+        $region = Read-Host "Enter the region "
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"region" : "ap-seoul-1"', (Write-Output '"region" : '$region))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"region" : "ap-seoul-1"', (Write-Output '"region" : '$region))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"region" : "ap-seoul-1"', (Write-Output '"region" : '$region))} | Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
+        $private_key_path = Read-Host "Enter the private_key_path(relative path) " 
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"private_key_path" : "./~~~~"', (Write-Output '"private_key_path" : '$private_key_path))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"private_key_path" : "./~~~~"', (Write-Output '"private_key_path" : '$private_key_path))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"private_key_path" : "./~~~~"', (Write-Output '"private_key_path" : '$private_key_path))} | Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
+        $subnet_ocid = Read-Host "Enter the subnet_ocid "
+        (Get-Content .\oci-ansible-server\oci_terraform_ansible_server.json) | ForEach-Object{$_ -Replace ('"subnet_ocid" : "ocid1.subnet.oc1.~~~~"', (Write-Output '"subnet_ocid" : '$subnet_ocid))} | Set-Content .\oci-ansible-server\oci_terraform_ansible_server.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json) | ForEach-Object{$_ -Replace ('"subnet_ocid" : "ocid1.subnet.oc1.~~~~"', (Write-Output '"subnet_ocid" : '$subnet_ocid))} | Set-Content .\oci-ansible-client-centos\oci_terraform_ansible_client_CentOS.json -Encoding ascii
+        (Get-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json) | ForEach-Object{$_ -Replace ('"subnet_ocid" : "ocid1.subnet.oc1.~~~~"', (Write-Output '"subnet_ocid" : '$subnet_ocid))} | Set-Content .\oci-ansible-client-windows\oci_terraform_ansible_client_Windows.json -Encoding ascii
     }
     else
     {
--- install.sh
+++ install.sh
@@ -9,14 +9,16 @@ if [ "$1" == "destroy" ]; then
     read -p "Select number: " num
     if [ $num -eq 1 ]; then
         terraform -chdir=oci-ansible-server/ destroy -var-file=oci_terraform_ansible_server.json -auto-approve
+        rm -rf ./oci-ansible-server/terraform.tfstate
     elif [ $num -eq 2 ]; then
         terraform -chdir=oci-ansible-client-centos/ destroy -var-file=oci_terraform_ansible_client_CentOS.json -auto-approve
+        rm -rf ./oci-ansible-client-centos/terraform.tfstate
     elif [ $num -eq 3 ]; then
         terraform -chdir=oci-ansible-client-windows/ destroy -var-file=oci_terraform_ansible_client_Windows.json -auto-approve
+        rm -rf ./oci-ansible-client-windows/terraform.tfstate
     else
         echo "Wrong number"
     fi
-    rm -rf terraform.tfstate
 
 elif [ "$1" == "apply" ]; then
     echo "Apply instance
@@ -24,7 +26,7 @@ elif [ "$1" == "apply" ]; then
     1. Server
     2. Linux client
     3. Windows client
-    4. Setup
+    4. Initial etup
     ---------------------------------------"
     read -p "Select number: " num
 
@@ -43,12 +45,29 @@ elif [ "$1" == "apply" ]; then
         terraform -chdir=oci-ansible-client-windows/ apply -var-file=oci_terraform_ansible_client_Windows.json -auto-approve
     elif [ $num -eq 4 ]; then
         read -p "Enter the tenancy_ocid: " tenancy_ocid
+        sed -i -r 's/"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"/"tenancy_ocid" : '$tenancy_ocid'/g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's/"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"/"tenancy_ocid" : '$tenancy_ocid'/g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's/"tenancy_ocid" : "ocid1.tenancy.oc1..~~~~"/"tenancy_ocid" : '$tenancy_ocid'/g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
         read -p "Enter the user_ocid: " user_ocid
+        sed -i -r 's/"user_ocid" : "ocid1.user.oc1..~~~~"/"user_ocid" : '$user_ocid'/g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's/"user_ocid" : "ocid1.user.oc1..~~~~"/"user_ocid" : '$user_ocid'/g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's/"user_ocid" : "ocid1.user.oc1..~~~~"/"user_ocid" : '$user_ocid'/g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
         read -p "Enter the fingerprint: " fingerprint
+        sed -i -r 's/"fingerprint" : "~~~~"/"fingerprint" : '$fingerprint'/g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's/"fingerprint" : "~~~~"/"fingerprint" : '$fingerprint'/g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's/"fingerprint" : "~~~~"/"fingerprint" : '$fingerprint'/g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
         read -p "Enter the region: " region
+        sed -i -r 's/"region" : "ap-seoul-1"/"region" : '$region'/g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's/"region" : "ap-seoul-1"/"region" : '$region'/g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's/"region" : "ap-seoul-1"/"region" : '$region'/g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
         read -p "Enter the private_key_path(relative path): " private_key_path
-        read -p "Enter the ssh_public_key: " ssh_public_key
+        sed -i -r 's%"private_key_path" : "./~~~~"%"private_key_path" : '$private_key_path'%g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's%"private_key_path" : "./~~~~"%"private_key_path" : '$private_key_path'%g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's%"private_key_path" : "./~~~~"%"private_key_path" : '$private_key_path'%g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
         read -p "Enter the subnet_ocid: " subnet_ocid
+        sed -i -r 's/"subnet_ocid" : "ocid1.subnet.oc1.~~~~"/"subnet_ocid" : '$subnet_ocid'/g' ./oci-ansible-server/oci_terraform_ansible_server.json
+        sed -i -r 's/"subnet_ocid" : "ocid1.subnet.oc1.~~~~"/"subnet_ocid" : '$subnet_ocid'/g' ./oci-ansible-client-centos/oci_terraform_ansible_client_CentOS.json
+        sed -i -r 's/"subnet_ocid" : "ocid1.subnet.oc1.~~~~"/"subnet_ocid" : '$subnet_ocid'/g' ./oci-ansible-client-windows/oci_terraform_ansible_client_Windows.json
     else
         echo "Wrong number"
     fi
